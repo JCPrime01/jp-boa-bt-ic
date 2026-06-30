5a54b719a6a2cfbb9a486a20001bf8931d09759d
JCPrime01 <vanzokiller@gmail.com>
2026-06-30 19:42:14 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -28,7 +28,9 @@
 
     const shareCode = '&shareCode=QY3MR187GKA'
     
-    const afp = '&afp=ic'
+    const afp = '&afp=bd'
+
+    const source = '&utm_medium=ic'
     
     const source = '&home=1'
 
